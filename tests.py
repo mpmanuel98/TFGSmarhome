c4b7559a78fbad0c1ab06baa0118a8173319a9f9
Manuel Marin Peral <mpmanuel98@gmail.com>
2020-02-25 18:38:29 +0100

    Face recognition example

--- tests.py
+++ tests.py
@@ -6,6 +6,11 @@ import requests
 import xml.etree.ElementTree as ET
 import io
 import time
+import face_recognition as FR
+from PIL import Image
+import cv2
+import numpy as np
+
 
 ##########################################
 ################## URLs ##################
@@ -27,4 +32,24 @@ url_dormitorio = "http://192.168.7.222:8891/cgi-bin/CGIProxy.fcgi?"
 url_pruebas_casa = "http://192.168.1.50:88/cgi-bin/CGIProxy.fcgi?"
 
 #ST.get_app_list()
-ST.set_app()
+#ST.set_app()
+
+#im = Image.open("imagenes/imagen1.jpg")
+#print(FR.detect_face("imagenes/imagenTest3.jpg"))
+
+faces, labels = FR.prepare_training_data("training-data")
+
+#create our LBPH face recognizer 
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+#or use EigenFaceRecognizer by replacing above line with 
+#face_recognizer = cv2.face.createEigenFaceRecognizer()
+
+#or use FisherFaceRecognizer by replacing above line with 
+#face_recognizer = cv2.face.createFisherFaceRecognizer()
+
+#train our face recognizer of our training faces
+face_recognizer.train(faces, np.array(labels))
+
+im = Image.open("imagenes/imagenTest.jpg")
+FR.predict(im)
