72cf60714fa843a1db5141b325629d17f28afd8b
Manuel Marín Peral <mpmanuel98@gmail.com>
2020-05-19 14:49:08 +0000

    Script update

--- start.cmd
+++ start.cmd
@@ -2,16 +2,21 @@
 
 echo Starting the processes to automate the illumination...
 
-python C:\Users\Manuel\GitRepos\TFGSmarhome\main_general.py -r kitchen
-python C:\Users\Manuel\GitRepos\TFGSmarhome\main_general.py -r bedroom
-python C:\Users\Manuel\GitRepos\TFGSmarhome\main_general.py -r livroom
+start /B py %CD%"\main_general.py" "-r" "kitchen"
+start /B py %CD%"\main_general.py" "-r" "bedroom"
+start /B py %CD%"\main_general.py" "-r" "livroom"
 
 set /p train=Do you want to obtain training images before launching the process to control de smartTV? (Y/N):
 
 IF "%train%"=="Y" (
-	python C:\Users\Manuel\GitRepos\TFGSmarhome\auto_capture.py -i 192.168.7.226 -p 8895
-)
+	echo Starting autocapture process...
+	start /wait py  %CD%"\auto_capture.py" "-i" "192.168.7.226" "-p" "8895"
 
-python C:\Users\Manuel\GitRepos\TFGSmarhome\main_tv.py -i 192.168.7.226 -p 8895
+	echo Starting the process to control the smartTV...
+	start /B py  %CD%"\main_tv.py" "-i" "192.168.7.226" "-p" "8895" 
+) ELSE (
+	echo Starting the process to control the smartTV...
+	start /B py  %CD%"\main_tv.py" "-i" "192.168.7.226" "-p" "8895"
+)
 
 pause
